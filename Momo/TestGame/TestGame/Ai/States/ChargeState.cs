bcdf76eadedfeead9f95c3dd21e3a17d4eda721e
Guntereno <GunterPete@hotmail.com>
2011-10-03 13:08:24 +0000

    Fixed crash bug.

--- Momo/TestGame/TestGame/Ai/States/ChargeState.cs
+++ Momo/TestGame/TestGame/Ai/States/ChargeState.cs
@@ -20,24 +20,27 @@ namespace TestGame.Ai.States
             // If touching the player, attack him
             AiEntity entity = GetEntity();
             SensedObject sensedObject = entity.SensoryData.SeePlayerSense;
-            DynamicGameEntity sensedEntity = sensedObject.SensedEntity;
-            Weapon weapon = entity.GetCurrentWeapon();
-            if ((weapon != null) && (sensedEntity != null))
+            if (sensedObject != null)
             {
-                float distSq = (entity.GetPosition() - sensedEntity.GetPosition()).LengthSquared();
-
-                float myRadSq = entity.GetContactRadiusInfo().RadiusSq;
-                float playerRadSq = sensedEntity.GetContactRadiusInfo().RadiusSq;
-
-                const float kRangeEpsilon = 21.0f;
-                const float kRangeEpsilonSq = kRangeEpsilon * kRangeEpsilon;
-                float triggerAmount = 0.0f;
-                if ((distSq - (myRadSq + playerRadSq)) <= kRangeEpsilonSq)
+                DynamicGameEntity sensedEntity = sensedObject.SensedEntity;
+                Weapon weapon = entity.GetCurrentWeapon();
+                if ((weapon != null) && (sensedEntity != null))
                 {
-                    const float kFullPower = 1.0f;
-                    triggerAmount = kFullPower;
+                    float distSq = (entity.GetPosition() - sensedEntity.GetPosition()).LengthSquared();
+
+                    float myRadSq = entity.GetContactRadiusInfo().RadiusSq;
+                    float playerRadSq = sensedEntity.GetContactRadiusInfo().RadiusSq;
+
+                    const float kRangeEpsilon = 21.0f;
+                    const float kRangeEpsilonSq = kRangeEpsilon * kRangeEpsilon;
+                    float triggerAmount = 0.0f;
+                    if ((distSq - (myRadSq + playerRadSq)) <= kRangeEpsilonSq)
+                    {
+                        const float kFullPower = 1.0f;
+                        triggerAmount = kFullPower;
+                    }
+                    weapon.Update(ref frameTime, triggerAmount, entity.GetPosition(), entity.FacingAngle);
                 }
-                weapon.Update(ref frameTime, triggerAmount, entity.GetPosition(), entity.FacingAngle);
             }
         }
     }
